3f731a2c68a9a0b6ae08053d7ec86aad54617912
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-11 10:44:30 +0200

    Add files via upload

---
+++ University Training/Assuit/Newcomers/1- Sheet #1/A. Say Hello With C++.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+using namespace std;
+
+int main() { IOS
+    string s; cin >> s;
+    cout << "Hello, " << s;
+}
