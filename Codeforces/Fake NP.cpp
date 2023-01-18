5b726cf90394c145573c696d9520ecc3fe117023
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-18 23:19:40 +0200

    Create Fake NP.cpp

---
+++ Codeforces/Fake NP.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define x first
+#define y second
+#define dd double
+#define ll long long
+#define ld long double
+#include<unordered_set>
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll l,r; cin >> l >> r;
+    if(l==r) cout << l;
+    else cout << 2;
+}
