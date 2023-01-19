be146e3c14fb84225f2fe5f9d1ffa342e93c191c
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-19 14:45:03 +0200

    Update Integer to Binary.cpp

--- Support/Integer to Binary.cpp
+++ Support/Integer to Binary.cpp
@@ -1,2 +1,9 @@
-string n_bin = bitset<32>(n_int).to_string();
-// or by dividing n_int by 2 and pushing back the remainder till n_int becomes 0
+string binary = bitset<32>(n).to_string();
+// or by dividing n by 2 and pushing back the remainder till n becomes 0
+// or
+void print_bits(ll n, ll size) {
+    if(!size) return;
+
+    print_bits(n>>1, size-1);
+    cout << (n&1);
+}
