51c513bd2e7ca2f07bd64d790d785018ee780818
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-21 13:59:48 +0200

    Create Iroha and Haiku (ABC Edition).cpp

---
+++ AtCoder/Iroha and Haiku (ABC Edition).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define x first
+#define y second
+#define dd double
+#define ll long long
+#define ld long double
+#include<unordered_set>
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll a,b,c; cin >> a >> b >> c;
+    map<ll,ll>m;
+    m[a]++;
+    m[b]++;
+    m[c]++;
+
+    if(m[5]=2 && m[7]==1) cout << "YES";
+    else cout << "NO";
+}
