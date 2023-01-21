852ad1b960d000e2a269a70d0ffb39ebb7140304
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-21 22:35:23 +0200

    Create Make Good.cpp

---
+++ Codeforces/Make Good.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define x first
+#define y second
+#define dd double
+#define ll long long
+#define ld long double
+#include<unordered_set>
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        ll sum1{},sum2{};
+        while(n--) {
+            ll num; cin >> num;
+            sum1 += num;
+            sum2 ^= num;
+        }
+        cout << 2 << endl << sum2 << " " << sum1+sum2 << endl;
+    }
+}
