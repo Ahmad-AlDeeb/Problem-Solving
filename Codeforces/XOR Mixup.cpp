2b5642a8a80b89e5292242be9b6a73e4996dd7ee
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-25 23:16:05 +0200

    Create XOR Mixup.cpp

---
+++ Codeforces/XOR Mixup.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        vector<ll>vec(n); for(auto &i:vec) cin >> i;
+
+        for(ll i{}; i<n; i++) {
+            ll total{};
+            for(ll j{}; j<n; j++)
+                if(j!=i) total ^= vec[j];
+            if(total == vec[i]) {cout << vec[i] << endl; break;}
+        }
+    }
+}
