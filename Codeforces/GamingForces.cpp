3cb062fdd2d5213bdafddafb13c9b9fe954f9a6b
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-25 19:28:51 +0200

    Create GamingForces.cpp

---
+++ Codeforces/GamingForces.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        ll ans{},ones{};
+        while(n--) {
+            ll health; cin >> health;
+            if(health > 1) ans++;
+            else ones++;
+        }
+        if(ones&1) ans += (ones/2)+1;
+        else ans += ones/2;
+
+        cout << ans << endl;
+    }
+}
