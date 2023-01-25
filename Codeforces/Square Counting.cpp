a629c4fa21627c2469aefd5bb4d296b253eb0615
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-25 23:17:51 +0200

    Create Square Counting.cpp

---
+++ Codeforces/Square Counting.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n,s; cin >> n >> s;
+        ll n2 = pow(n,2);
+        ll ans = s/n2;
+        cout << ans << endl;
+    }
+}
