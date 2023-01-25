2bec2aedad0109a4e811d97bfe60f2f4e977959f
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-25 22:10:06 +0200

    Update GCD & LCM.cpp

--- Support/GCD & LCM.cpp
+++ Support/GCD & LCM.cpp
@@ -1,7 +1,7 @@
-long long gcd(long long a, long long b) {
+ll gcd(ll a, ll b) {
     if(b != 0) return gcd(b, a%b);
     else return a;
 }
-long long lcm(long long a, long long b) {
+ll lcm(ll a, ll b) {
     return a*b / gcd(a,b);
 }
