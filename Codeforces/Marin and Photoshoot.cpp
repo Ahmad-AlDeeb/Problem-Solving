32ad45ca29e35c1f91b0891425676b3127e46fa8
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-29 14:03:05 +0200

    Create Marin and Photoshoot.cpp

---
+++ Codeforces/Marin and Photoshoot.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+bool is_prime(ll n) {
+    for(ll i{2}; i*i<=n; i++)
+        if(n%i == 0) return false;
+    return true;
+}
+
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; string s; cin >> n >> s;
+        ll ones{},zeros{},count{};
+        for(auto c:s) {
+            if(c=='0') {
+                if(zeros) {
+                    if(ones < 2) count += 2-ones;
+                    ones = 0;
+                }
+                else {zeros=1; ones=0;}
+            }
+            else if(c=='1') ones++;
+        }
+        cout << count << endl;
+    }
+}
