bc6ff0bb2faf57686477057facc39637471c3112
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-01-31 18:53:45 +0200

    Create Division?.cpp

---
+++ Codeforces/Division?.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+
+int main() { IOS // freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        if(n >= 1900) cout << "Division 1";
+        else if(n >= 1600) cout << "Division 2";
+        else if(n >= 1400) cout << "Division 3";
+        else cout << "Division 4";
+        cout << endl;
+    }
+}
