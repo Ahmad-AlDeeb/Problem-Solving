a0df5d9ce1b40539f4dc523cf8422e182358b5b0
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-02 23:56:42 +0200

    Create Levko and Table.cpp

---
+++ Levko and Table.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll n,k; cin >> n >> k;
+    vector<ll>vec(n); vec.back() = k;
+
+    for(ll i{},pos{}; i<n; i++,pos++) {
+        for(ll j{}; j<n; j++,pos++) {
+            pos %= n;
+            cout << vec[pos] << " ";
+        } cout << endl;
+    }
+
+}
