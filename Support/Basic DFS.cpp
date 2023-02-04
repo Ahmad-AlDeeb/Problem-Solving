4b1c6dadd617812699511ce5de56b55d791422a8
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-05 00:38:22 +0200

    Update Basic DFS.cpp

--- Support/Basic DFS.cpp
+++ Support/Basic DFS.cpp
@@ -1,25 +1,8 @@
-int n,e;
-vector< vector<int> > adj;
-vector<bool> visited;
+vector<vector<ll>>adj;
+vector<bool>vis;
+void dfs(ll node) {
+    vis[node]=true;
 
-void dfs(int node) {
-    visited[node] = true;
-    
-    for(ll i{}; i<adj[node].size; i++) {
-        int child = adj[node][i];
-        if(!visited[child]) dfs(child);
-    }
-}
-
-
-int main() { IOS
-    cin >> n >> e;
-    adj = vector<vector<int>>(n);
-    visited = vector<bool>(n);
-    
-    for(int i{}; i<n; i++) {
-        int from,to;
-        cin >> from >> to;
-        adj[from-1].push_back(to-1)
-    }
+    for(auto child:adj[node])
+        if(!vis[child]) dfs(child);
 }
