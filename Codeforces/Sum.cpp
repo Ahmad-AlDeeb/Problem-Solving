6f760f66a9d56b71f64da470620c41205bdd54e9
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-07 22:53:05 +0200

    Create Sum.cpp

---
+++ Codeforces/Sum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll t; cin>>t;
+    while(t--){
+        ll a,b,c; cin >> a >> b >> c;
+        if(a==b+c or b==a+c or c==a+b) cout << "YES";
+        else cout << "NO";
+        cout << endl;
+    }
+}
