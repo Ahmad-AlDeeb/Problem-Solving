db39fff86cc51d800ffdf6bb37c2285ae5334290
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-08 17:27:39 +0200

    Create Summer Camp.cpp

---
+++ Codeforces/Summer Camp.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll n; cin >> n;
+    string s;
+    for(ll i{1}; s.size()<1000; i++) s += to_string(i);
+    cout << s[n-1];
+}
