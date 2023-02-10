e6a38f2fef3d5711920671a05217d256170c03f3
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-10 19:41:01 +0200

    Create Reachable Numbers.cpp

---
+++ Codeforces/Reachable Numbers.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll n; cin >> n;
+    set<ll>ans; ans.insert(n); n++;
+    while(n>10) {
+        if(n%10 == 0) n /= 10;
+        else {ans.insert(n); n++;}
+    }
+
+    for(ll i{1}; i<10; i++) ans.insert(i);
+
+    cout << ans.size();
+}
