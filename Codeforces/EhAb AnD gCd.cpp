7921754cb4bbc5dbb8e30986e91964b1b2bb4191
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-02-26 20:00:55 +0200

    Create EhAb AnD gCd.cpp

---
+++ Codeforces/EhAb AnD gCd.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+
+ll gcd(ll a, ll b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+ll lcm(ll a, ll b) {
+    return a*b / gcd(a,b);
+}
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        cout << 1 << " " << n-1 << endl;
+    }
+}
