9cdf67b842d2a7625e55bf6cb0d5b4fae171b390
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-03-07 06:17:16 +0200

    Create Vanya and Cards.cpp

---
+++ Codeforces/Vanya and Cards.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e5;
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+
+bool is_valid(ld num) {
+    ll int_num = num;
+    if(num == int_num) return true;
+    return false;
+}
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll n,x; cin >> n >> x;
+    ll sum{};
+    while(n--) {ll num; cin >> num; sum += num;}
+    if(sum != 0)
+        cout << ceil(abs(sum)/(dd)x);
+    else cout << 0;
+}
