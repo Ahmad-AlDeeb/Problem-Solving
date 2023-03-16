c6fa1cc77c0a5afe0fd089a9ef4e06e7c71a05b4
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-03-16 10:53:02 +0200

    Update Double Comparison.cpp

--- Support/Double Comparison.cpp
+++ Support/Double Comparison.cpp
@@ -1,4 +1,4 @@
-int comp_double(double a, double b) {
-    if(fabs(a-b) <= 1e-10) return 0;
-    return a<b? -1:1;
+int comp_double(ld a, ld b) {
+    if(fabs(a-b) <= 1e-10) return -1;
+    return a>b? 1:0;
 }
