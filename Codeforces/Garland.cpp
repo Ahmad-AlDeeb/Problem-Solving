6b9ba74b3cb5d1cae8534c4776be409c826e60e7
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-03-24 03:46:48 +0200

    Create Garland.cpp

---
+++ Codeforces/Garland.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e9+7;
+ll power(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+
+
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        string s; cin >> s;
+        set<char>se; vector<ll>f(10);
+        for(auto c:s) {
+            se.insert(c);
+            f[c-'0']++;
+        }
+        if(se.size() == 1) cout << -1;
+        else if(se.size() == 4) cout << 4;
+        else {
+            ll maxi{};
+            for(auto freq:f) maxi=max(maxi,freq);
+            cout << maxi*2;
+        }
+        cout << endl;
+    }
+}
