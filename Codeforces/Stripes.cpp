c734fe592721520c58c30117eff52ddb156b7b72
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-10 17:40:17 +0200

    Create Stripes.cpp

---
+++ Codeforces/Stripes.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e9+7;
+ll power(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+
+
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll t; cin >> t;
+    while(t--) {
+        vector<vector<char>>map(8,vector<char>(8));
+        for(auto &i:map) for(auto &j:i) cin >> j;
+
+        char ans{'.'};
+        for(ll i{}; i<8; i++) {
+            ll R{};
+            for(ll j{}; j<8; j++)
+                if(map[i][j]=='R') R++;
+            if(R==8) ans = 'R';
+        }
+        if(ans == 'R') cout << ans; else cout << 'B';
+        cout << endl;
+    }
+
+}
