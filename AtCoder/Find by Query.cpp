469fbf60fbf3827d99f11ca7526bf07badc0fc79
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-22 15:13:14 +0200

    Create Find by Query.cpp

---
+++ AtCoder/Find by Query.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e9+7;
+ll power(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+string letters{" abcdefghijklmnopqrstuvwxyz"};
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll n; cin >> n;
+
+    ll l{},r{n-1}, mid{(r-l)/2};
+    while(true) {
+        cout << "? " << mid+1 << endl; cout.flush();
+        ll x; cin >> x;
+
+        if(!x) l=mid,mid = l+(r-l)/2;
+        else r=mid, mid = r-(r-l)/2;
+
+        if(r-l==1) {
+            cout << "! " << l+1 << endl; cout.flush();
+            break;
+        }
+    }
+}
