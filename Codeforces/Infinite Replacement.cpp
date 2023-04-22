7180fda0aca5124a90d189a359103f7717a7eaa3
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-22 13:36:22 +0200

    Create Infinite Replacement.cpp

---
+++ Codeforces/Infinite Replacement.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e9+7;
+ll power(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+string letters{" abcdefghijklmnopqrstuvwxyz"};
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int tc; cin >> tc;
+    while(tc--) {
+        string s,t; cin >> s >> t;
+        if(t.find('a') == string::npos) {
+            ll total{};
+            ll n = s.size();
+            for(ll r{}; r<=s.size(); r++) {
+                ld sum{1};
+                for(int i{1}; i <= r; i++)
+                    sum = sum * (n-r+i) / i;
+                total += (ll)sum;
+            }
+            cout << total;
+        }
+        else {
+            if(t.size()==1) cout << 1;
+            else cout << -1;
+        }
+        cout << endl;
+
+    }
+
+}
