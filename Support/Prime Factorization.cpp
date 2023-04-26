45b81775b0beb0a26c38321152115a271b4832f7
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-26 19:21:34 +0200

    Delete Prime Factorization.cpp

--- Support/Prime Factorization.cpp
+++
@@ -1,9 +0,0 @@
-vector<ll> p_fact(ll x) {
-    ll i{2}; vector<ll> p;
-    for(; i*i <= x; i++) {
-        if(x%i == 0) p.pb(i);
-        while(x%i == 0) x /= i;
-    }
-    if(x != 1) p.push_back(x);
-    return p;
-}
