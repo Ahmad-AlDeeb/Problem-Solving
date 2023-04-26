22b320f7f739733ac625665ac653a4806e81dc06
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-26 18:17:45 +0200

    Update Prime Factorization.cpp

--- Support/Prime Factorization.cpp
+++ Support/Prime Factorization.cpp
@@ -1,10 +1,9 @@
-vector<int> p_fact(int x) {
-    int i{2}; vector<int> p;
-    while(i*i <= x; i++)
-        while(x%i == 0) {
-            x /= i; // we keep dividing as long as i divides x
-            p.push_back(i); // and we push to factors vector
-        }
-    if(x != 1) p.push_back(i);
+vector<ll> p_fact(ll x) {
+    ll i{2}; vector<ll> p;
+    for(; i*i <= x; i++) {
+        if(x%i == 0) p.pb(i);
+        while(x%i == 0) x /= i;
+    }
+    if(x != 1) p.push_back(x);
     return p;
 }
