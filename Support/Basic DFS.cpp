fcd34418c0c4c233382b97533f591bc03adc1a7e
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-26 17:32:17 +0200

    Update Basic DFS.cpp

--- Support/Basic DFS.cpp
+++ Support/Basic DFS.cpp
@@ -1,8 +1,20 @@
 vector<vector<ll>>adj;
 vector<bool>vis;
+vector<ll> topsort;
+ll n;
+
 void dfs(ll node) {
     vis[node]=true;
 
     for(auto child:adj[node])
         if(!vis[child]) dfs(child);
+
+    topsort.pb(node); // DAG // Other Way Indegree / Outdegree
+}
+
+ll connected_components_cnt() {
+    ll cnt{};
+    for(ll i{}; i<n; i++)
+        dfs(i),cnt++;
+    return cnt;
 }
