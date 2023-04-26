e4008c02328af47290befee525dfa9c1df7e6b77
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-26 18:26:34 +0200

    Delete Binary Search.cpp

--- Support/Binary Search.cpp
+++
@@ -1,7 +0,0 @@
-bool binary_search(vector<ll>&vec, ll x, int ll, int ll) {
-    if(left > right) return false;
-    int mid {(left+right)/2};
-    if(vec[mid] == x) return true;
-    else if(x < vec[mid]) return binary_search(vec, x, left, mid-1);
-    else                  return binary_search(vec, x, mid+1, right);
-}
