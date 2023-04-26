eeacd422ae003e9549be56ff21c50c931a4c605f
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-04-26 16:55:18 +0200

    Create Polycarp Recovers the Permutation.cpp

---
+++ Codeforces/Polycarp Recovers the Permutation.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define F first
+#define S second
+#define dd double
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+const ll inf = 1e17;
+const ll M = 1e9+7;
+ll power(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+vector<ll> di {1,-1,1,-1};
+vector<ll> dj {1,1,-1,-1};
+string letters{" abcdefghijklmnopqrstuvwxyz"};
+ll gcd(ll a, ll b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+vector<ll> p_fact(ll x) {
+    ll i{2}; vector<ll> p;
+    for(; i*i <= x; i++) {
+        if(x%i == 0) p.pb(i);
+        while(x%i == 0) x /= i;
+    }
+    if(x != 1) p.push_back(x);
+    return p;
+}
+
+int main() { IOS
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ll tc; cin >> tc;
+    while(tc--) {
+        ll n; cin >> n;
+        vector<ll>vec(n); for(auto &i:vec) cin >> i;
+        if(vec.front()!=n && vec.back()!=n) cout << -1;
+        else {
+            cout << n << " ";
+            reverse(all(vec));
+            for(auto &num:vec) if(num!=n) cout << num << " ";
+        }
+        cout << endl;
+    }
+
+}
