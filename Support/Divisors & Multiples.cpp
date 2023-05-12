c06915becf91a7a6348974011fe95042c69cdced
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-05-12 15:29:01 +0300

    Update Divisors & Multiples.cpp

--- Support/Divisors & Multiples.cpp
+++ Support/Divisors & Multiples.cpp
@@ -2,16 +2,15 @@
 vector<ll>divisors;
 for(int i{1}; i*i<=x; i++) 
     if(x%i == 0) {
-        divisors.push_back(x);
+        divisors.push_back(i);
         if(i*i != x) divisors.push_back(x/i);
     }
 ///////////////////////////////////  Multiples /////////////////////////////////////////
-vector<vector<ll>>multiples(MAX);
-void init() {
-    for(ll fact{}; fact<MAX; fact++)
-        for(ll multi{fact}; multi<MAX; multi+=fact)
-            multiples[multi].pb(fact);
-}
+vector<vector<ll>>multiples(MAX+1);
+for(ll div{}; div<MAX; div++)
+    for(ll multi{div}; multi<=MAX; multi+=div)
+        multiples[multi].pb(div);
+
 
 
 
