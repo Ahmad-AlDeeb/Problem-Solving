c46fd4026fddd275fde923b897e87a7b0298ae77
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-05-21 18:15:21 +0300

    Create Sumar and the Floating Rocks.cpp

---
+++ HackerRank/Sumar and the Floating Rocks.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER y1
+ *  3. INTEGER x2
+ *  4. INTEGER y2
+ */
+int gcd(int a, int b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+int solve(int x1, int y1, int x2, int y2) {
+    int min_x = min(x1,x2);
+    x1 -= min_x;
+    x2 -= min_x;
+    int min_y = min(y1,y2);
+    y1 -= min_y;
+    y2 -= min_y;
+    return gcd(max(x1,x2),max(y1,y2))-1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int x1 = stoi(first_multiple_input[0]);
+
+        int y1 = stoi(first_multiple_input[1]);
+
+        int x2 = stoi(first_multiple_input[2]);
+
+        int y2 = stoi(first_multiple_input[3]);
+
+        int result = solve(x1, y1, x2, y2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
