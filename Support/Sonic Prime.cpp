7ba1dcc2a8e1aab2e26939d2994c041bebbbdec9
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-06-22 18:06:31 +0300

    Delete Sonic Prime.cpp

--- Support/Sonic Prime.cpp
+++
@@ -1,9 +0,0 @@
-// Source: CP-4 Book Part (2) Page (19)
-bool isPrime(ll N) { // good enough prime test
-  if (N < _sieve_size) return bs[N]; // O(1) for small primes
-
-  for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i)
-    if (N%p[i] == 0)
-      return false;
-  return true; // slow if N = large prime
-} // note: only guaranteed to work for N <= (last prime in vll p)^2
