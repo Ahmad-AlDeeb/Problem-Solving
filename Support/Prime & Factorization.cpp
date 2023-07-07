fbb0728e649f572783131c9eb2fd7c5fe9fdb2a5
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-08 00:57:44 +0300

    Create Prime & Factorization.cpp

---
+++ Support/Prime & Factorization.cpp
@@ -0,0 +1,92 @@
+ll SS; vll P; bitset<100000010> BS;
+void sieve(ll sz) { // (1e7 < 1s) <-- O(n loglogn)
+    SS = sz+1;
+    BS.set(); BS[0]= BS[1]=false;
+
+    for(ll i{2}; i<SS; ++i) if(BS[i]) {
+            for(ll j{i*i}; j<SS; j+=i)
+                BS[j]=false;
+            P.pb(i);
+        }
+}
+bool prime(ll n) { // 1e13 ==> (work for N <= (last prime in vll P)^2)
+    if(n < SS) return BS[n];
+    for(ll i{}; i<P.size() and P[i]*P[i]<=n; ++i)
+        if(n%P[i] == 0) return false;
+    return true;
+}
+vll PF(ll n) { // 1e13 ==> (work for N <= (last prime in vll P)^2)
+    vll factors;
+    for(ll i{}; i<P.size() and P[i]*P[i]<=n; ++i) {
+        while(n%P[i] == 0)
+            n/=P[i], factors.pb(P[i]);
+    }
+
+    if(n!=1) factors.pb(n);
+    return factors;
+}
+vll PF() {
+    vll PF_cnt(10000007);
+    for(ll i{2}; i<=PF_cnt.size(); ++i)
+        if(!PF_cnt[i]) // if true then i is prime
+            for(ll j{i}; j<=PF_cnt.size(); j+=i)
+                ++PF_cnt[j];
+    return PF_cnt;
+}
+vll div(ll x) {
+    vll div;
+    for(ll i{1}; i*i<=x; i++)
+        if(x%i == 0) {
+            div.pb(i);
+            if(i*i != x) div.pb(x/i);
+        }
+    return div;
+}
+ll div_cnt(ll n) { // 1e13 ==> (work for N <= (last prime in vll P)^2)
+    ll cnt{1};
+    for(ll i{}; i<P.size() and P[i]*P[i]<=n; ++i) {
+        ll power{};
+        while(n%P[i] == 0) n/=P[i], ++power;
+        cnt *= power+1;
+    }
+    return (n!=1)? 2*cnt:cnt;
+}
+ll div_sum(ll n) { // 1e13 ==> (work for N <= (last prime in vll P)^2)
+    ll sum{1};
+    for(ll i{}; i<P.size() and P[i]*P[i]<=n; ++i) {
+        ll multi{P[i]}, total{1};
+        while(n%P[i] == 0)
+            n/=P[i], total+=multi, multi*=P[i];
+        sum *= total;
+    }
+
+    if(n!=1) sum *= (n+1); // N^2-1/N-1 = N+1
+    return sum;
+}
+vector<vll> multi(ll x) { // 2*1e6
+    vector<vll> multi(x+1);
+    for(ll div{}; div<=x; div++)
+        for(ll i{div}; i<=x; i+=div)
+            multi[i].pb(div);
+    return multi;
+}
+ll euler(ll n) { // 1e13 ==> (work for N <= (last prime in vll P)^2)
+    ll co{n};
+    for(ll i{}; i<P.size() and P[i]*P[i]<=n; ++i) {
+        if(n%P[i] == 0) co -= co / P[i];
+        while(n%P[i] == 0) n/=P[i];
+    }
+
+    if(n!=1) co -= co/n;
+    return co;
+}
+vll euler() {
+    vll co(10000007);
+    for(ll i{1}; i<co.size(); ++i)
+        co[i]=i;
+    for(ll i{2}; i<co.size(); ++i)
+        if(co[i]==i) // if  true then i is prime
+            for(ll j{i}; j<=co.size(); j+=i)
+                co[j] = (co[j]/i) * (i-1);
+    return co;
+}
