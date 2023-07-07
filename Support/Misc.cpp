f1cb57fd35289a39f8894b4201de09648accf1a1
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-08 00:57:04 +0300

    Create Misc.cpp

---
+++ Support/Misc.cpp
@@ -0,0 +1,29 @@
+void coordinate_compress(vll &axis, ll start=2, ll step=2) {
+    set<ll> s (axis.begin(), axis.end());
+    map<ll,ll> index;
+
+    ll i{};
+    for(auto val:s)
+        index[val] = start + (step * i++);
+
+    for(auto &val:axis)
+        val = index[val];
+}
+void coordinate_compress(vll &axis, vll &values, map<ll,ll> &index, ll start=2, ll step=2) {
+    for(auto &val:axis)
+        index[val] = 0;
+    values.resize(start + step*axis.size());
+
+    ll i{};
+    for(auto &p:index) {
+        p.S = start + (step * i++);
+        values[p.S] = p.F;
+    }
+
+    for(auto &val:axis)
+        val = index[val];
+}
+pair<ld,ld> quad(ld a, ld b, ld c) {
+    ld sq = sqrt(pow(b,2LL)-4*a*c);
+    return {-b+(sq)/2*a, -b-(sq)/2*a};
+}
