1227e53b50ef22f1537490df346f70f6c34ac048
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-11 10:58:24 +0300

    Update Misc.cpp

--- Support/Misc.cpp
+++ Support/Misc.cpp
@@ -27,3 +27,17 @@ pair<ld,ld> quad(ld a, ld b, ld c) {
     ld sq = sqrt(pow(b,2LL)-4*a*c);
     return {-b+(sq)/2*a, -b-(sq)/2*a};
 }
+// Difference between 2 squares
+ll a,b;
+void get_squares(ll n) {
+    for(ll i = sqrt(n); i >= 1; i--)
+        if(n%i == 0) {
+            ll c = i;   //a-b
+            ll d = n/i; //a+b
+            if((c+d)%2 == 0 && (c-d)%2 == 0) {
+                a = (c + d) / 2;
+                b = (d - c) / 2;
+                return;
+            }
+        }
+}
