a266773bfb1bf498f2bf46eda643ceff708e9d5e
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-11 11:13:53 +0300

    Update Misc.cpp

--- Support/Misc.cpp
+++ Support/Misc.cpp
@@ -52,3 +52,14 @@ void get_squares(ll n) {
             }
         }
 }
+    /////////////// Stress testing /////////////////
+   while(true) {
+//        ll n = 2601;
+       ll n = rand()%10+1;
+       if(fast(n) == slow(n)) cout << n << " OK!\n";
+       else {
+           cout << "NOT MATCHED!\n";
+           cout << n << endl << slow(n) << " " << fast(n);
+           break;
+       }
+   }
