fd06f000c6a4b765ea58f31bef5ac933a8b903e2
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-11 11:04:07 +0300

    Delete Ordered Set.cpp

--- Support/Ordered Set.cpp
+++
@@ -1,4 +0,0 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<long long, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>
