06ce06ba4314d405833a52c007090e699082c9c0
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-11 22:39:23 +0300

    Update Factorial.cpp

--- Support/Factorial.cpp
+++ Support/Factorial.cpp
@@ -25,3 +25,27 @@ ll comb(ll n, ll r) {
     }
     return ans;
 }
+// CP-4 Combination calculation from Binomial Coefficient section page 36 (2nd book)
+ll mod_pow(ll b, ll p, ll m) {
+    if(!p) return 1;
+
+    ll ans = mod_pow(b,p/2,m);
+    ans = mod(ans*ans,m);
+
+    if(p&1) ans = mod(ans*b,m);
+    return ans;
+}
+const ll P = 1e9+7;
+ll inv(ll a) {
+    return mod_pow(a,P-2,P);
+}
+ll Fact[100010];
+ll C(ll n, ll k) {
+    if(n<k) return 0;
+    return ( ((Fact[n]*inv(Fact[k]))%P) * inv(Fact[n-k]) ) % P;
+}
+void ini_fact() { // O(n)
+    Fact[0]=1;
+    for(ll i{1}; i<100010; i++)
+        Fact[i] = (Fact[i-1]*i) % P;
+}
