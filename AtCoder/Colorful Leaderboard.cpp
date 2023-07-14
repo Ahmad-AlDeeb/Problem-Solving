59fc4543f3cbba16fd5228980b1f15aedfa9ea84
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-14 10:10:32 +0300

    Create Colorful Leaderboard.cpp

---
+++ AtCoder/Colorful Leaderboard.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll MAX = 1e18;
+const ll M = 1e9+7;
+const ld EPS = 1e-19;
+const ll SZ {101};
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n; cin >> n;
+    ll unique{};
+    set<ll>color;
+    while(n--) {
+        ll x; cin >> x;
+        if(x<=399) color.insert(1);
+        else if(x<=799) color.insert(2);
+        else if(x<=1199) color.insert(3);
+        else if(x<=1599) color.insert(4);
+        else if(x<=1999) color.insert(5);
+        else if(x<=2399) color.insert(6);
+        else if(x<=2799) color.insert(7);
+        else if(x<=3199) color.insert(8);
+        else unique++;
+    }
+    ll mn = color.size();
+    cout << max(1LL,mn)  << " " << mn+unique;
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    while(tc--) {
+        fast();
+//        cout << endl;
+    }
+}
