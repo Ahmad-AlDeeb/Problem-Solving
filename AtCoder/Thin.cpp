ba94ae0bd13c088e9fe6e1c4dacbb4f40903e993
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-16 12:40:57 +0300

    Create Thin.cpp

---
+++ AtCoder/Thin.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const string letters{"abcdefghijklmnopqrstuvwxyz"};
+const ll MAX = 1e18;
+const ll M = 1e9+7;
+const ld EPS = 1e-19;
+const ll SZ {101};
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll h,w; cin >> h >> w;
+    char grid[h][w];
+    for(ll i{}; i<h; i++) {
+        for(ll j{}; j<w; j++) cin >> grid[i][j];
+        for(ll j{}; j<w; j++) cout << grid[i][j];
+        cout << endl;
+        for(ll j{}; j<w; j++) cout << grid[i][j];
+        if(i!=h-1) cout << endl;
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    while(tc--) {
+        fast();
+//        cout << endl;
+    }
+}
