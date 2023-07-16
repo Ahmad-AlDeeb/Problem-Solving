0c57a040cb3abd85a0f20deac803a34a2a61cb96
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-16 10:38:12 +0300

    Create Between a and b ... .cpp

---
+++ AtCoder/Between a and b ... .cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const string letters{"abcdefghijklmnopqrstuvwxyz"};
+const ll MAX = 1e18;
+const ll M = 1e9+7;
+const ld EPS = 1e-19;
+const ll SZ {101};
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll a,b,x; cin >> a >> b >> x;
+    ll ans1 = b/x+1;
+    ll ans2 = a/x+1;
+    ll ans = ans1-ans2;
+    if(a%x==0) ans++;
+    cout << ans;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    while(tc--) {
+        fast();
+//        cout << endl;
+    }
+}
