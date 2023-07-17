389471a51d4a1323b96791c3eaa6607af5c2f5d4
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-17 21:13:39 +0300

    Create Ultimate Army.cpp

---
+++ Codeforces/Ultimate Army.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const vll di {0,0,1,-1};
+const vll dj {1,-1,0,0};
+const string letters{"abcdefghijklmnopqrstuvwxyz"};
+const ll MAX = 1e18;
+const ll M = 1e9+7;
+const ld EPS = 1e-19;
+const ll SZ {101};
+const ld PI = 3.14159265358979323846;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n; string s; cin >> n >> s;
+
+    stack<ll> boss;
+    string first;
+
+    ll i{};
+    for(; isdigit(s[i]); i++)
+        first += s[i];
+    boss.push(stoll(first));
+
+    map<ll,ll> ans;
+    ans[boss.top()]=0;
+
+    for(i++; i<s.size(); i++) {
+        if(isdigit(s[i])) {
+            string num;
+            while(isdigit(s[i])) num += s[i++];
+            ll sold = stoll(num);
+
+            ans[sold]=boss.top();
+            if(s[i]=='(') boss.push(sold);
+        }
+        else if(s[i]=='(') continue;
+        else if(s[i]==')') boss.pop();
+    }
+    for(ll i{1}; i<=n; i++)
+        cout << ans[i] << " ";
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    while(tc--) {
+        fast();
+//        cout << endl;
+    }
+}
