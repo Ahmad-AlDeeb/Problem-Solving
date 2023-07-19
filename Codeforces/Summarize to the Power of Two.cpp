8b7e768a759f60e91d32d0dc8b693730266bbe82
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-19 13:32:40 +0300

    Update Summarize to the Power of Two.cpp

--- Codeforces/Summarize to the Power of Two.cpp
+++ Codeforces/Summarize to the Power of Two.cpp
@@ -1,55 +1,82 @@
-#include<string>
-#include<iostream>
-#include<sstream>
-#include<algorithm>
-#include<cmath>
-#include<iomanip>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<deque>
-#include<set>
-#include<unordered_set>
-#define start first
-#define endd second
-#define dd double
-#define ll long long
-#define ld long double
+#include <bits/stdc++.h>
+using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-
-int main() { IOS
-    vector<ll>powers{1};
-    while(powers.back() < 2*10e9) powers.push_back(powers.back()*2);
-
-    ll n; cin >> n;
-    vector<ll>vec(n); for(auto &i:vec) cin >> i; sort(vec.begin(),vec.end());
-
-    ll invalid{};
-    for(ll i{}; i<n; i++) {
-        bool success{false};
-
-        for(auto power:powers) {
-            ll tar {power-vec[i]};
-            if(tar > 0) {
-                auto it = lower_bound(vec.begin(),vec.end(),tar);
-                if(it!=vec.end() && *it==tar) {
-                    if(i != it-vec.begin()) success=true;
-                    else {
-                        it++;
-                        if(it!=vec.end() && *it==tar) success=true;
-                    }
-                }
-            }
-
-        }
-
-        if(!success) invalid++;
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const vll di {0,0,1,-1};
+const vll dj {1,-1,0,0};
+const string letters{"abcdefghijklmnopqrstuvwxyz"};
+const ll MAX = 1e18;
+const ll M = 1e9+7;
+const ld EPS = 1e-19;
+const ll SZ {101};
+const ld PI = 3.14159265358979323846;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
     }
-    cout << invalid;
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n; cin >> n;
+    map<ll,ll> f;
+    vll v(n); for(auto &i:v) cin >> i, f[i]++;
+    sort(all(v)); ll mx = v.back();
+
+    ll ans{};
+    for(ll i{}; i<n; i++) {
+        bool cool{false};
+        for(ll power{1}; power<=mx*2; power*=2) {
+            if(v[i]>=power) continue;
+            ll tar = power-v[i];
+            if(binary_search(all(v),tar)) {
+                if(v[i]==tar && f[v[i]]==1) continue;
+                cool=true;
+            }
+        }
+        if(!cool) ans++;
+    }
+    cout << ans;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    while(tc--) {
+        fast();
+//        cout << endl;
+    }
 }
