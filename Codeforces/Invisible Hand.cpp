4e2df61cf669fb416e2aaf225daf92387228acc2
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-29 16:12:56 +0300

    Create Invisible Hand.cpp

---
+++ Codeforces/Invisible Hand.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n,m; cin >> n >> m;
+    vector<pair<ll,ll>> v;
+    vll a(n),b(m); for(auto &i:a) cin >> i; for(auto &i:b) cin >> i;
+    for(auto sell:a) v.emplace_back(sell,0);
+    for(auto buy:b) v.emplace_back(buy,1);
+    sort(all(v));
+
+    ll buy{m},sell{};
+    for(auto val:v) {
+        if(!val.S) {
+            sell++;
+            if(sell>=buy) {cout << val.F; return;}
+        }
+        else {
+            buy--;
+            if(sell>=buy) {cout << val.F+1; return;}
+        }
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+        fast();
+//        cout << endl;
+    }
+}
