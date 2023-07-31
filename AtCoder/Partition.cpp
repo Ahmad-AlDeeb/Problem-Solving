7eb938f2f2e8a8a878fb3ad2d0dc0c9cb99664d2
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-31 08:44:07 +0300

    Create Partition.cpp

---
+++ AtCoder/Partition.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 10000007;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+vll div(ll x) {
+    vll div;
+    for(ll i{1}; i*i<=x; i++)
+        if(x%i == 0) {
+            div.pb(i);
+            if(i*i != x) div.pb(x/i);
+        }
+    sort(all(div));
+    return div;
+}
+ll gcd(ll a, ll b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+ll lcm(ll a, ll b) {
+    return a / gcd(a,b) * b;
+}
+void fast() {
+    ll n,m; cin >> n >> m;
+    vll d = div(m);
+
+    ll ans{1};
+    for(ll i=d.size()-1; i>=0; i--) {
+        ll rem = m - (d[i]*(n-1));
+        if(rem<=0) continue;
+
+        ans = max(ans, gcd(d[i],rem));
+    }
+    cout << ans;
+//    cout << gcd(m/n, (m/n)+m%n);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+        fast();
+//        cout << endl;
+    }
+}
