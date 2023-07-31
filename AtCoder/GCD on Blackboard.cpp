58a65191fc86287d073715c180a20b6f7b3bef63
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-31 10:47:37 +0300

    Create GCD on Blackboard.cpp

---
+++ AtCoder/GCD on Blackboard.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 10000007;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+ll gcd(ll a, ll b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+ll lcm(ll a, ll b) {
+    return a / gcd(a,b) * b;
+}
+void fast() {
+    ll n; cin >> n;
+    vll v(n); for(auto &i:v) cin >> i;
+
+    vll l(n+1);
+    for(ll i{}; i<n; i++)
+        l[i+1] = gcd(l[i], v[i]);
+    vll r(n+1);
+    for(ll i{n}; i>0; i--)
+        r[i-1] = gcd(r[i],v[i-1]);
+
+    ll ans{};
+    for(ll i{}; i<n; i++)
+        ans = max(ans,gcd(l[i],r[i+1]));
+    cout << ans;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+        fast();
+//        cout << endl;
+    }
+}
