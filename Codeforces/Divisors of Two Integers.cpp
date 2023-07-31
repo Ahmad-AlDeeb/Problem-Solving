9e68c696c60aa0e2de4a8ffa33cf811bc0eb2358
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-07-31 19:06:02 +0300

    Create Divisors of Two Integers.cpp

---
+++ Codeforces/Divisors of Two Integers.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 10000007;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+vll div(ll x) {
+    vll div;
+    for(ll i{1}; i*i<=x; i++)
+        if(x%i == 0) {
+            div.pb(i);
+            if(i*i != x) div.pb(x/i);
+        }
+    sort(all(div));
+    return div;
+}
+void fast() {
+    ll n; cin >> n;
+    map<ll,ll> f;
+    vll v(n); for(auto &i:v) cin >> i, f[i]++;
+    sort(all(v));
+    ll a = v.back();
+    vll a_factors = div(a);
+
+    for(ll i{n-2}; i>=0; i--) {
+        vll factors = div(v[i]);
+        bool cool{true};
+        for(auto fact:factors)
+            if(binary_search(all(a_factors),fact) and f[fact]==1)
+                cool=false;
+        if(cool) {cout << a << " " << v[i]; return;}
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+        fast();
+//        cout << endl;
+    }
+}
