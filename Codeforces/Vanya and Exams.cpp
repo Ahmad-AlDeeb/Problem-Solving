8638e08d201f4ec4908fcc91381c0a35bb31f15f
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-08-05 23:02:12 +0300

    Create Vanya and Exams.cpp

---
+++ Codeforces/Vanya and Exams.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 11;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n,r,avg; cin >> n >> r >> avg;
+    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<>> pq;
+    ll sum{};
+    for(ll i{}; i<n; i++) {
+        ll a,b; cin >> a >> b;
+        sum += a;
+        if(a<r) pq.emplace(b,a);
+    }
+    ll tar {avg*n};
+    ll degree {tar-sum};
+
+    ll ans{};
+    while(degree>0) {
+        auto top = pq.top(); pq.pop();
+
+        ll diff {r-top.S};
+        ll mn = min(diff,degree);
+        
+        ans += mn * top.F;
+        degree-=mn;
+    }
+    cout << ans;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
