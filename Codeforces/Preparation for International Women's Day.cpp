e29c4415a66d3575261e367318e228a0805d991a
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-08-07 15:37:45 +0300

    Create Preparation for International Women's Day.cpp

---
+++ Codeforces/Preparation for International Women's Day.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 11;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n,k; cin >>n>>k;
+    map<ll,ll> f;
+    for(ll i{}; i<n; i++) {
+        ll x; cin >> x;
+        f[x%k]++;
+    }
+
+    ll ans {f[0]};
+    if(f[0]&1) ans--;
+
+    for(ll i{1},j{k-1}; i<=j; i++,j--) {
+        if(i==j) {
+            ans += f[i];
+            if(f[i]&1) ans--;
+        }
+        else ans += min(f[i],f[j]) * 2;
+    }
+    cout << ans;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
