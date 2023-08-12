2851de1273acc4a854e463e84e2335f623280665
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-08-12 16:23:07 +0300

    Create LOWER.cpp

---
+++ AtCoder/LOWER.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n,q; string s; cin >> n >> s >> q;
+    vector<pair<ll,pair<ll,char>>> query(q);
+    for(auto &i:query) {
+        cin >> i.F >> i.S.F >> i.S.S;
+        if(i.F==1) s[i.S.F-1] = i.S.S;
+    }
+
+    ll i{q-1};
+    for(; i>=0; i--)
+        if(query[i].F != 1) break;
+
+    if(i!=-1) {
+        if(query[i].F == 2)
+            for(auto &c:s) c=(char)tolower(c);
+        else
+            for(auto &c:s) c=(char)toupper(c);
+    }
+
+    for(++i; i<q; i++)
+        s[query[i].S.F-1] = query[i].S.S;
+
+    cout << s;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
