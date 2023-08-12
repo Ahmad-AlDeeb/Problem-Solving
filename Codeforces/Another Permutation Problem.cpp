8728127428e94be0fd1eb92781f054439f55735f
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-08-12 21:18:58 +0300

    Create Another Permutation Problem.cpp

---
+++ Codeforces/Another Permutation Problem.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+ll n;
+ll get_ans(vll &v) {
+    ll sum{},mx{-MAX};
+    for(ll j{1}; j<=n; j++)
+        sum+=j*v[j-1], mx=max(mx,j*v[j-1]);
+    return sum-mx;
+}
+void fast() {
+    cin >> n;
+    vll v(n);
+    for(ll i{}; i<n; i++)
+        v[i] = i+1;
+
+    ll ans{-MAX};
+    for(ll i{}; i<n; i++) {
+        vll v2(v);
+        for(ll j{i},dec{}; j<n; j++,dec++)
+            v2[j]=n-dec;
+        ans = max(ans,get_ans(v2));
+    }
+    cout << ans;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+}
