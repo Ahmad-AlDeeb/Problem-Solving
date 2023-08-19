61eb1b2fb615c5bcb4e9e79c351ac9cae123f657
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-08-19 19:28:06 +0300

    Create Flavors.cpp

---
+++ AtCoder/Flavors.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n; cin >> n;
+    vector<pair<ll,ll>> cups(n); for(auto &i:cups) cin >> i.S >> i.F;
+    sort(all(cups), greater<>());
+
+    auto mx = cups[0];
+    auto mx2 = cups[1];
+    if(mx.S != mx2.S) cout << mx.F+mx2.F;
+    else {
+        ll ans1 {mx.F+(mx2.F/2)};
+        ll ans2 {mx.F};
+        for(ll i{2}; i<n; i++)
+            if(cups[i].S != mx.S) {
+                ans2 += cups[i].F;
+                break;
+            }
+        cout << max(ans1,ans2);
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
