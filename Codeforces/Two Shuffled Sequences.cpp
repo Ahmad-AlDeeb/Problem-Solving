cb78d0eabc8d637d4b30894f04ac56a1213f9ea6
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-09-03 23:46:44 +0300

    Create Two Shuffled Sequences.cpp

---
+++ Codeforces/Two Shuffled Sequences.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+void fast() {
+    ll n; cin >> n;
+
+    set<ll> s1,s2;
+    bool cool{true};
+    for(ll i{}; i<n; i++) {
+        ll x; cin >> x;
+        if(!s1.count(x)) s1.insert(x);
+        else if(!s2.count(x)) s2.insert(x);
+        else cool = false;
+    }
+
+    if(!cool) cout << "NO";
+    else {
+        vll v2; for(auto num:s2) v2.pb(num);
+        sort(all(v2),greater<>());
+
+        cout << "YES\n";
+        cout << s1.size() << endl;
+        for(auto num:s1) cout << num << " ";
+        cout << endl;
+        cout << v2.size() << endl;
+        for(auto num:v2) cout << num << " ";
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
