44a5ec4ce3bb491389b81288992786d2d931f490
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-09-08 19:12:54 +0300

    Create Plus Minus Permutation.cpp

---
+++ Codeforces/Plus Minus Permutation.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e5+5;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+ll gcd(ll a, ll b) {
+    if(b != 0) return gcd(b, a%b);
+    else return a;
+}
+ll lcm(ll a, ll b) {
+    return a / gcd(a,b) * b;
+}
+ll n, common{};
+ll sum_x(ll x) {
+    ll cnt = n/x;
+    cnt = max(cnt-common, 0LL);
+    ll start = n-cnt+1;
+    return (start+n) * cnt / 2;
+}
+ll sum_y(ll x) {
+    ll cnt = n/x;
+    cnt = max(cnt-common, 0LL);
+    return (1+cnt) * cnt / 2;
+}
+void fast() {
+    ll x,y; cin >> n >> x >> y;
+    common = n/lcm(x,y);
+    cout << sum_x(x) - sum_y(y);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+}
