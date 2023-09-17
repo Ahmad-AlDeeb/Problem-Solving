332a7547a1c488d76d7f37b426bc6b96ae498089
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-09-17 06:41:50 +0300

    Create Longest Palindrome.cpp

---
+++ AtCoder/Longest Palindrome.cpp
@@ -0,0 +1,79 @@
+Longest Palindrome.cpp
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////
+const ll N = 1e7;
+const ll M = 1e9+7;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///////////////////////////////////////////////// ===== Solution ===== /////////////////////////////////////////////////
+bool is_palindrome(string &s) {
+    ll sz = (ll)s.size();
+    for(ll i{},j{sz-1}; i<=j; i++,j--)
+        if(s[i] != s[j]) return false;
+    return true;
+}
+void fast() {
+    string s; cin >> s;
+    ll sz = (ll)s.size();
+    ll ans{1};
+    for(ll i{}; i<sz; i++) {
+        string s2;
+        for(ll j{i}; j<sz; j++) {
+            s2 += s[j];
+            if(is_palindrome(s2))
+                ans = max(ans, (ll)s2.size());
+        }
+    }
+    cout << ans;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+//    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+//        cout << endl;
+    }
+}
