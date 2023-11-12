f775061f5a2c8a66cd758e6c206cf011b68d7569
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-11-12 11:04:11 +0200

    Create Computer Game.cpp

---
+++ Codeforces/Computer Game.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////////
+const ll N = 1e7+7;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///█████████████████████████████████████████████████████ Solution ██████████████████████████████████████████████████████
+ll k,n,a,b;
+bool valid(ll x) {
+    ll cur{k}, tar{n};
+    cur -= x*a;
+    tar -= x;
+    cur -= tar*b;
+
+    return cur > 0;
+}
+void fast() {
+    cin >> k>>n>>a>>b;
+
+    ll mx{k/b};
+    if(k%b == 0) mx--;
+    if(mx < n) {cout << -1; return;}
+
+    ll l{},r{n}, ans;
+    while(l<=r) {
+        ll mid {l + (r-l)/2};
+        if(valid(mid)) l=mid+1, ans=mid;
+        else           r=mid-1;
+    }
+    cout << ans;
+}
+///█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+}
