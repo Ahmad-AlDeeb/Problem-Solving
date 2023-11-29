2595ab62071988462816df1d6ec15c97c115d10d
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-11-29 10:08:32 +0200

    Create Laura and Operations.cpp

---
+++ Codeforces/Laura and Operations.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////////
+const ll N = 1e7+7;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///█████████████████████████████████████████████████████ Solution ██████████████████████████████████████████████████████
+void fast() {
+    ll a,b,c; cin >> a>>b>>c;
+    // very small number bug?
+    ll d1 = abs(b-c);
+    ll d2 = abs(a-c);
+    ll d3 = abs(a-b);
+
+    bool ans1 = d1%2==0;
+    bool ans2 = d2%2==0;
+    bool ans3 = d3%2==0;
+    cout << ans1 << " " << ans2 << " " << ans3;
+}
+///█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+}
+
+
