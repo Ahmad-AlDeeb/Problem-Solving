9ee2f51eaaf26895de5883d4c7609dcc4ee5d477
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-12-19 19:35:22 +0200

    Create Preparing for the Contest.cpp

---
+++ Codeforces/Preparing for the Contest.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////////
+const ll N = 1e7+7;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///█████████████████████████████████████████████████████ Solution ██████████████████████████████████████████████████████
+void fast() {
+    ll n,k; cin >> n >> k;
+    if(k == 0) {
+        for(ll i{n}; i>0; i--) cout << i << " ";
+        return;
+    }
+
+    ll cnt{};
+    vll v{1};
+    for(ll i{2}; i<=n; i++) {
+        if(cnt < k-1) {
+            v.pb(i);
+            cnt++;
+        }
+        else {v.pb(n); break;}
+    }
+
+    while(v.size() != n)
+        v.pb(v.back()-1);
+
+    for(auto num:v) cout << num << " ";
+}
+///█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+
+}
+
+
