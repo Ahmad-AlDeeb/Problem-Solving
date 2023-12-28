4c5ac1340220ec579944f73188ff7fbe395cc388
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2023-12-28 19:09:03 +0200

    Create Unnatural Language Processing.cpp

---
+++ Codeforces/Unnatural Language Processing.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////////
+const ll N = 1e7+7;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///█████████████████████████████████████████████████████ Solution ██████████████████████████████████████████████████████
+bool is_v(char c) {
+    return c=='a' or c=='e';
+}
+void fast() {
+    ll n; string s; cin >> n >> s;
+    bool c{false},v{false};
+    vector<string> ans;
+    string sub;
+
+    for(ll i{n-1}; i>=0; i--) {
+        char cur = s[i];
+
+        if(is_v(cur)) {
+            sub += cur;
+            v=true;
+        }
+        else {
+            sub += cur;
+            if(v) {
+                reverse(all(sub));
+                ans.pb(sub);
+                sub.clear();
+                v=false;
+            }
+        }
+    }
+    reverse(all(ans));
+
+    string output;
+    for(auto str:ans) {
+        output += str;
+        output += '.';
+    }
+    output.pop_back();
+    cout << output;
+}
+///█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+
+}
+
+
