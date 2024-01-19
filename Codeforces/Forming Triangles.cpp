45de09788389bd743ee3a965c977fc019741e028
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-01-19 14:03:08 +0200

    Create Forming Triangles.cpp

---
+++ Codeforces/Forming Triangles.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+///////////////////////////////////////
+const ll N = 1e7+7;
+const ll M = 998244353;
+const ll MAX = 1e18;
+const ld EPS = 1e-19;
+ll pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans *= x;
+        x *= x;
+        y /= 2;
+    }
+    return ans;
+}
+ll big_pow(ll x, ll y) {
+    ll ans{1};
+    while(y) {
+        if(y&1) ans = ((ans%M)*(x%M)) % M;
+        x = ((x%M) * (x%M)) %M;
+        y/=2;
+    }
+    return ans;
+}
+bool is_int(ld num) {
+    return num == (ll)num;
+}
+int cmp_d(ld a, ld b) {
+    if(fabs(a-b) <= EPS) return -1;
+    return a>b? 1:0;
+}
+ll mod(ll x, ll m){
+    return (x%m +m) % m;
+}
+///█████████████████████████████████████████████████████ Solution ██████████████████████████████████████████████████████
+void fast() {
+    ll n; cin >> n;
+    map<ll,ll>f;
+    vll v(n); for(auto &i:v) cin >> i, f[i]++;
+
+    vector<pair<ll,ll>> v2;
+    for(auto p:f) v2.emplace_back(p.F, p.S);
+
+    ll ans{};
+    for(auto p:v2)
+        ans += (p.S * (p.S-1) * (p.S-2)) / 6;
+
+    ll sum{v2.front().S};
+    for(ll i{1}; i<v2.size(); i++) {
+        auto cur {v2[i]};
+        ans += sum * ( (cur.S*(cur.S-1)) / 2 );
+        sum += cur.S;
+    }
+    cout << ans;
+}
+///█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int main() { IOS
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+    ll tc{1};
+    cin >> tc;
+    for(ll i{1}; i<=tc; i++) {
+//        cout << "Case " << i << ": ";
+        fast();
+        cout << endl;
+    }
+
+}
+
+
