ae6f36a6258206c62fc6ee31edeb98ca3f7ac1b2
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-03-02 07:20:25 +0200

    Create 704. Binary Search.cpp

---
+++ Leetcode/704. Binary Search.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(!binary_search(nums.begin(), nums.end(), target)) return -1;
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
