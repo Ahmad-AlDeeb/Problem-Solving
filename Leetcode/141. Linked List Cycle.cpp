b11801eb37ef242c426c9e5ac0e19474ce8f8abf
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-04-02 13:20:08 +0200

    Create 141. Linked List Cycle.cpp

---
+++ Leetcode/141. Linked List Cycle.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) 
+            return false;
+
+        ListNode* fast = head;
+
+        while(fast && fast->next) {
+            head = head->next;
+            fast = fast->next->next;
+            if(head == fast) 
+                return true;
+        }
+        
+        return false;
+    }
+};
