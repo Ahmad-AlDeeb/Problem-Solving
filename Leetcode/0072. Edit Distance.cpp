694a66e4c48ad84240b7d948c8f2803a67e00762
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-10-03 18:28:30 +0300

    Update 0072. Edit Distance.cpp

--- Leetcode/0072. Edit Distance.cpp
+++ Leetcode/0072. Edit Distance.cpp
@@ -26,6 +26,39 @@ int getMin(int i, int j) {
     return answer = min({changeOperation, insertOperation, deleteOperation});
 }
 
+void printSteps(int i, int j) {
+    if(i == n1 && j == n2) {
+        return;
+    }
+    if(i == n1) {
+        cout << w2.substr(j) << " was inserted at the end of " << w1 << endl;
+        return;
+    }
+    else if(j == n2) {
+        cout << w1.substr(i) << " was inserted at the end of " << w2 << endl;
+        return;
+    }
+
+    int optimal = getMin(i, j);
+
+    int changeOperation = 1 + getMin(i + 1, j + 1);
+    int insertOperation = 1 + getMin(i    , j + 1);
+    int deleteOperation = 1 + getMin(i + 1, j    );
+
+    if(changeOperation == optimal) {
+        cout << "Changed " << w1[i] << " in " << w1 << " to " << w2[j] << endl;
+        printSteps(i + 1, j + 1);
+    }
+    else if(insertOperation == optimal) {
+        cout << "Inserted " << w2[j] << " before " << w1[i] << endl;
+        printSteps(i, j + 1);
+    }
+    else {
+        cout << "Deleted " << w1[i] << " in " << w1 << endl;
+        printSteps(i + 1, j);
+    }
+}
+
 public:
     int minDistance(string word1, string word2) {
         w1 = word1;
@@ -34,6 +67,7 @@ public:
         n2 = w2.size();
         memset(memory, -1, sizeof(memory));
 
+        printSteps(0, 0);
         return getMin(0, 0);
     }
 };
