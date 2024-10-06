7618d54999bc8697ee4605bb9a2e58a198680714
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-10-06 08:50:48 +0300

    Update 0047. Permutations II.cpp

--- Leetcode/0047. Permutations II.cpp
+++ Leetcode/0047. Permutations II.cpp
@@ -36,3 +36,62 @@ public:
         return answer;
     }
 };
+
+
+// Dr Mostafa solution without using unordered_set or set (just by using frequency array !)
+/*
+One simple generic way for permutations is simply the frequency approach
+
+Assume we have input {4, 4, 5, 6, 6, 6}
+	So frequency is:
+		4: 2
+		5: 1
+		6: 3
+
+	We have an output of 6 values
+	In each position, just pick one of the unique values; initially 4, 5, 6
+	In this way; we try e.g. 4 only once
+
+In other words, we apply our backtrack were we try all our unique choices per idx
+
+Note; there are another approach based on while loop to skip similar numbers
+	https://leetcode.com/problems/permutations-ii/discuss/18599/C%2B%2B-clean-recursive-easy-solution-beats-92
+ */
+
+vector<int> nums;
+unordered_map<int, int> freq_mp;
+vector<vector<int>> perm_list;
+vector<int> cur_perm;
+
+void permute_bt(int idx) {
+	if (idx == (int) nums.size()) {
+		perm_list.push_back(cur_perm);
+		return;
+	}
+
+	for (auto [value, freq] : freq_mp) if (freq) {
+		freq_mp[value] -= 1;
+		cur_perm[idx] = value;
+
+		permute_bt(idx + 1);
+
+		freq_mp[value] += 1;
+		cur_perm[idx] = -1;
+	}
+}
+
+class Solution {
+public:
+	vector<vector<int>> permuteUnique(vector<int> &nums_) {
+        nums = nums_;
+        perm_list.clear();
+        freq_mp.clear();
+
+		cur_perm = vector<int>(nums.size(), -1);
+		for (int i : nums)
+			freq_mp[i]++;
+
+		permute_bt(0);
+		return perm_list;
+	}
+};
