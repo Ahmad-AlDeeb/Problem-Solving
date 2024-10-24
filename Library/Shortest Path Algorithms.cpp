9d1506d92b711267b9b09f6bcf20b6493f1fbb73
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-10-24 10:37:34 +0300

    Update Shortest Path Algorithms.cpp

--- Library/Shortest Path Algorithms.cpp
+++ Library/Shortest Path Algorithms.cpp
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
-// Floyd-Warshal: all pairs | DP | detect negative cycles | adjacency matrix | O(n3)
+// Floyd-Warshal: APSP | DP | detect negative cycles | adjacency matrix | O(n3)
 void floyd(vector<vector<int>>& shortestPath) {
   int n = shortestPath.size();
   
@@ -32,7 +32,7 @@ int main() {
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
-// Bellman-Ford: single source | DP | detect negative cycles | edge list | O(n2) O(n)
+// Bellman-Ford: SSSP | DP | detect negative cycles | edge list | O(EV) O(n)
 const int INF = (int) 1e6;
 struct Edge {
 	int from, to, w;
@@ -66,4 +66,47 @@ int main() {
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
-// Dijkstra: single source | Greedy | non-negative weights only
+// Dijkstra: SSSP | Greedy | non-negative weights only | O(E logV)
+const int INF = (int) 1e6;
+struct Edge {
+	int to, w;
+
+	Edge(int to, int w) : to(to), w(w) {
+	}
+
+	bool operator <(const Edge &e) const {
+		return w > e.w;
+	}
+};
+vector<int> Dijkstra(const vector<vector<Edge>> &adjList, int n, int src) {
+	vector<int> dist(n, INF);
+	vector<bool> vis(n);
+	dist[src] = 0;
+
+	priority_queue<Edge> q;	// small to large
+	q.push(Edge(src, 0));
+
+	while (!q.empty()) {
+		// Get node with the minimum distance
+		Edge minEdge = q.top();
+		int minIndex = minEdge.to;
+		q.pop();
+
+		// The same node might come several times
+		if (vis[minIndex])
+			continue;
+
+		// Relax with the outgoing edges of the min node
+		for (const auto &edge : adjList[minIndex]) {
+			int to = edge.to, weight = edge.w;
+
+			if (dist[to] > dist[minIndex] + weight) {
+				dist[to] = dist[minIndex] + weight;
+				// Add new edges
+				q.push( {to, dist[to] });
+			}
+		}
+		vis[minIndex] = true;
+	}
+	return dist;
+}
