70c44520ff233eb2e106a035294b4216c59233db
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-12-25 09:50:22 +0200

    Update 0376. Wiggle Subsequence.cpp

--- Leetcode/0376. Wiggle Subsequence.cpp
+++ Leetcode/0376. Wiggle Subsequence.cpp
@@ -1,3 +1,5 @@
+////////////////////////////////////////////////////////////
+///////////////////// Greedy Solution /////////////////////
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
@@ -21,5 +23,44 @@ public:
             }
         }
         return maxLength;
+    }
+};
+
+
+////////////////////////////////////////////////////////////
+/////////////////////// DP Solution ///////////////////////
+class Solution {
+    int cache[1000 + 5][2];
+    vector<int> nums;
+    enum CHOICE {UP = true, DOWN = false};
+    int size;
+
+    int getMaxLength(int index, bool choice) {
+        if(index >= size) {
+            return 0;
+        }
+
+        int &maxLength = cache[index][choice];
+        if(maxLength != -1) {
+            return maxLength;
+        }
+
+        int takeOption{};
+        if(choice == UP && nums[index] > nums[index - 1] ||
+           choice == DOWN && nums[index] < nums[index - 1]) {
+                takeOption = 1 + getMaxLength(index + 1, !choice);
+        }
+        int skipOption = getMaxLength(index + 1, choice);
+
+        return maxLength = max(takeOption, skipOption);
+    }
+
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        this->nums = nums;
+        size = this->nums.size();
+        memset(cache, -1, sizeof(cache));
+
+        return 1 + max(getMaxLength(1, UP), getMaxLength(1, DOWN));
     }
 };
