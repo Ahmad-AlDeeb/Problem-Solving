c911c1028dcd44841f5c2a496dcbeb8c822b0403
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2024-12-29 20:27:59 +0200

    Create 1991. Find the Middle Index in Array.cpp

---
+++ Leetcode/1991. Find the Middle Index in Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> prefix(nums);
+        
+        for(int i{1}; i < size; ++i) {
+            prefix[i] += prefix[i - 1];
+        }
+
+        if(prefix[size - 1] - nums[0] == 0) {
+            return 0;
+        }
+        
+        for(int i{1}; i < size; ++i) {
+            int leftSum = prefix[i - 1];
+            int rightSum = prefix[size - 1] - prefix[i];
+            
+            if(leftSum == rightSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
