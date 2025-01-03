81f50ed2b1a55c5763ed27719ed96a4c35b82c27
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2025-01-03 22:37:07 +0200

    Create 0287. Find the Duplicate Number.cpp

---
+++ Leetcode/0287. Find the Duplicate Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find the intersection point of the two pointers
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // DIDN"T UNDERSTAND THIS PART !!! ARGGGG! MATH MAGIC ? HOW DO THEY MEET AT ENTRANCE!?
+        // Find the entrance of the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
