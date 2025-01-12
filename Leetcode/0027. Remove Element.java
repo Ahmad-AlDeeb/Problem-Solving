e8002d13b378b0213c4d06373aa6b76ca02147fc
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2025-01-12 06:24:48 +0200

    Create 0027. Remove Element.java

---
+++ Leetcode/0027. Remove Element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        for(int right = 0; right < nums.length; ++right) {
+            if(nums[right] != val) {
+                nums[left] = nums[right];
+                left++; 
+            }
+        }
+        return left;
+    }
+}
