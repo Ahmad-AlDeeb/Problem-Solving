b85d114023cf39e44f094334f4f815cd69cdc058
Ahmad Al-Deeb <122436546+Ahmad-AlDeeb@users.noreply.github.com>
2025-01-12 09:19:35 +0200

    Create 2037. Minimum Number of Moves to Seat Everyone.java

---
+++ Leetcode/2037. Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int minMoves = 0;
+        for(int i = 0; i < seats.length; ++i) {
+            minMoves += Math.abs(seats[i] - students[i]);
+        }
+        return minMoves;
+    }
+}
